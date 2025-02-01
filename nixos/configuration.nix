9edbe83346dc39c1c32f409dde0e7ed4868bc525
dmkhitaryan <davtot111@icloud.com>
2025-02-01 15:42:36 +0100

    Ironing out system service inconsistencies.

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -268,11 +268,12 @@
 
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
-  # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  # };
+  programs.mtr.enable = true;
+  programs.gnupg.agent = {
+    enable = true;
+    enableSSHSupport = true;
+    pinentryPackage = pkgs.pinentry.curses;
+  };
 
   # Steam setup.
   programs.steam = {
